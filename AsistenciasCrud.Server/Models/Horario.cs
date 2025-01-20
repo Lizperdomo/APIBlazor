3ebcabd63d6088aa6207ebcd0b2a91e9ee157ea6
lizperdomo <lizperdomo35586@gmail.com>
2025-01-19 20:39:44 -0600

    Agregar archivos de proyecto.

---
+++ AsistenciasCrud.Server/Models/Horario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsistenciasCrud.Server.Models;
+
+public partial class Horario
+{
+    public int IdHorario { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public TimeOnly HoraInicial { get; set; }
+
+    public TimeOnly HoraFinal { get; set; }
+
+    public string DiasLaborales { get; set; } = null!;
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+}
---
+++ AsistenciasCrud.Server/Models/RegistroasistenciaContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsistenciasCrud.Server.Models;
+
+public partial class RegistroasistenciaContext : DbContext
+{
+    public RegistroasistenciaContext()
+    {
+    }
+
+    public RegistroasistenciaContext(DbContextOptions<RegistroasistenciaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Asistencia> Asistencias { get; set; }
+
+    public virtual DbSet<Horario> Horarios { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=registroasistencia;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Asistencia>(entity =>
+        {
+            entity.HasKey(e => e.IdAsistencia).HasName("PK__asistenc__3956DEE6D77FE8D7");
+
+            entity.ToTable("asistencias");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Asistencia)
+                .HasForeignKey(d => d.IdUsuario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__asistenci__IdUsu__4BAC3F29");
+        });
+
+        modelBuilder.Entity<Horario>(entity =>
+        {
+            entity.HasKey(e => e.IdHorario).HasName("PK__horarios__1539229B94B71033");
+
+            entity.ToTable("horarios");
+
+            entity.Property(e => e.DiasLaborales).HasMaxLength(100);
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Horarios)
+                .HasForeignKey(d => d.IdUsuario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__horarios__IdUsua__4E88ABD4");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__usuarios__5B65BF97C638FB1B");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.ApellidoM).HasMaxLength(40);
+            entity.Property(e => e.ApellidoP).HasMaxLength(40);
+            entity.Property(e => e.Correo).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(40);
+            entity.Property(e => e.Telefono).HasMaxLength(10);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
